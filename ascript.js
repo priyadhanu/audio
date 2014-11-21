0a2156b2d6dd53593deafbcb558bdb6188e677b7
Gowda <dhanu@hampi.(none)>
2014-11-21 16:22:19 +0200

    hello once

--- ascript.js
+++ ascript.js
@@ -1,4 +1,11 @@
+
+$('document').ready(function () {
+    $('#play').click(function () {
 document.addEventListener("deviceready", onDeviceReady, false);
+	
+  });
+});  
+
 function onDeviceReady() {
     playAudio();
 }
@@ -15,6 +22,26 @@ function playAudio() {
             console.log("playAudio():Audio Error: " + err);
         }
     );
-    // Play audio
+    // Play audio */
+    my_media.play();
+}
+/* document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    playAudio();
+}
+
+function playAudio() {
+    // Play the audio file at url
+    var my_media = new Media('/android_asset/www/testaudio.mp3',
+        // success callback
+        function () {
+            console.log("playAudio():Audio Success");
+        },
+        // error callback
+        function (err) {
+            console.log("playAudio():Audio Error: " + err);
+        }
+    );
+    // Play audio */
     my_media.play();
 }
